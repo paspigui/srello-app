37e9d630c1490c4c160eb430f1020441ed921585
lutfi <paspigui@gmail.com>
2024-08-13 17:57:00 +0200

    Home page & other stuff

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,23 @@
+import { Logo } from "@/components/logo"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <div className=" fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
+      <div className=" md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div>
+          <Button variant="default" className="mr-2">
+            <Link href="./sign-in">Sign In</Link>
+          </Button>
+          <Button variant="secondary">
+            <Link href="./sign-up">
+              Sign Up
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className=" text-center">
-      <h1>Welcome to Srello!</h1>
-    </div>
-  );
-}
